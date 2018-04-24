e3d47120f90f24cca975328a8becc50a98673020
tejaksk123 <35579904+tejaksk123@users.noreply.github.com>
2018-04-24 00:33:01 -0500

    code

---
+++ source/three.js/examples/myserver2.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+  passphrase:'titanic'
+};
+
+// Create a service (the app object is just a callback).
+var app = express();
+
+// Create an HTTP service.
+http.createServer(app).listen(80);
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(443);
